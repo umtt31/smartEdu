9981d342320542d7920407941b7aef4d5aa6d365
umtt31 <umutcan.adiguzel@outlook.com>
2023-10-19 22:31:29 +0300

    giris sayfasi ve giris sistemi yapildi

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcyrpt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -6,5 +7,23 @@ exports.createUser = async (req, res) => {
     res.status(201).render('register', { user, page_name: 'register' });
   } catch (error) {
     res.status(400).json({ status: 'fail', error });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await User.findOne({ email }).then((user) => {
+      if (user) {
+        bcyrpt.compare(password, user.password, (err, result) => {
+          if (result) {
+            // USER SESSION
+            res.status(200).send('You are logged in');
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).send({ status: 'fail', error });
   }
 };
